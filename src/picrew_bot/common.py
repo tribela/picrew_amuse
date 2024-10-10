5c8da275f48a661e7de592b8834baa809a614cd1
Jeong Arm <kjwonmail@gmail.com>
2024-10-11 01:10:28 +0900

    Match default storage location to docker volume

--- src/picrew_bot/common.py
+++ src/picrew_bot/common.py
@@ -2,7 +2,7 @@ import os
 
 DEFAULT_FONT_PATH = '/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf'
 
-STORAGE_PATH = os.getenv('PICREW_STORAGE_PATH', 'storage')
+STORAGE_PATH = os.getenv('PICREW_STORAGE_PATH', 'state')
 QUESTION_IMAGE_PATH = os.path.join(STORAGE_PATH, 'question.webp')
 ANSWER_IMAGE_PATH = os.path.join(STORAGE_PATH, 'answer.webp')
 STATE_PATH = os.path.join(STORAGE_PATH, 'state.json')
